041594098961580dc5303a9acd43235bbdd7047e
dmi3s <dmi3evsv@gmail.com>
2010-04-22 02:03:02 +0400

    Oops, find mistake in 1.16 (but it was correct)

--- 1/1.16.scm
+++ 1/1.16.scm
@@ -9,7 +9,7 @@
 (define (fast-exp b n )
   (define (fast-exp-iter b n a)
     (cond ((= n 0) a)
-          ((even? n) (fast-exp-iter b (/ n 2) (square a)))
+          ((even? n) (fast-exp-iter (square b) (/ n 2) a))
           (else (fast-exp-iter b (- n 1) (* a b)))))
   (fast-exp-iter b n 1))
                      
