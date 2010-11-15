e1e3a559f356a844a0d4bee8426ada07f92b5c96
dmi3s <dmi3evsv@gmail.com>
2010-11-15 13:28:39 +0300

    1.21

---
+++ 1/1.21.scm
@@ -0,0 +1,15 @@
+#lang planet neil/sicp
+
+(define (smallest-divisor n)
+  (find-divisor n 2))
+
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (+ test-divisor 1)))))
+
+(define (divides? a b)
+  (= (remainder b a) 0))
+
+(define (square n)
+  (* n n))
