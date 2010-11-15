2289cfa41891ce79bc4934a1a81cd05a665c3223
dmi3s <dmi3evsv@gmail.com>
2010-11-16 02:20:52 +0300

    1.32

---
+++ 1/1.32.scm
@@ -0,0 +1,19 @@
+#lang planet neil/sicp
+
+(define (accumulate combiner null-value term a next b)
+  (define (iter a result)
+    (if (> a b)
+        result
+        (iter (next a) (combiner result (term a)))))
+  (iter a null-value))
+
+(define (sum term a next b)
+  (accumulate + 0 term a next b))
+
+(define (product term a next b)
+  (accumulate * 1 term a next b))
+
+(define (inc k) (+ k 1))
+(define (identity x) x)
+    
+    
