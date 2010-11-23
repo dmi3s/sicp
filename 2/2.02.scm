b461e52eda243837f8cb4ecd966136228ece8f6c
dmi3s <dmi3evsv@gmail.com>
2010-11-23 04:30:17 +0300

    2.02

---
+++ 2/2.02.scm
@@ -0,0 +1,38 @@
+#lang planet neil/sicp
+
+(define (make-point x y)
+  (cons x y))
+
+(define (x-point p)
+  (car p))
+
+(define (y-point p)
+  (cdr p))
+
+(define (make-segment start end)
+  (cons start end))
+
+(define (start-segment s)
+  (car s))
+
+(define (end-segment s)
+  (cdr s))
+
+(define (mid-segment s)
+  (define (average a b)
+    (/ (+ a b) 2.0))
+  (make-point
+   (average (x-point (start-segment s)) (x-point (end-segment s)))
+   (average (y-point (start-segment s)) (y-point (end-segment s)))))
+
+(define (print-point p)
+  (newline)
+  (display "(")
+  (display (x-point p))
+  (display ",")
+  (display (y-point p))
+  (display ")"))
+
+(define (main) 
+  (print-point (mid-segment (make-segment (make-point 1 2) (make-point 3 4)))))
+
