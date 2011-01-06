1e27047c83e63eb479f7e5a272bc3092eadac79a
dmi3s <dmi3evsv@gmail.com>
2011-01-06 22:00:44 +0300

    2.14

---
+++ 2/2.14.scm
@@ -0,0 +1,59 @@
+#lang planet neil/sicp
+
+(define (make-interval a b) (cons a b))
+
+(define (upper-bound i)
+  (max (cdr i) (car i)))
+
+(define (lower-bound i)
+  (min (cdr i) (car i)))
+
+(define (mul-interval x y)
+  (let ((p1 (* (lower-bound x) (lower-bound y)))
+        (p2 (* (lower-bound x) (upper-bound y)))
+        (p3 (* (upper-bound x) (lower-bound y)))
+        (p4 (* (upper-bound x) (upper-bound y))))
+    (make-interval (min p1 p2 p3 p4)
+                   (max p1 p2 p3 p4))))
+
+(define (div-interval x y)
+  (if (> (* (upper-bound y) (lower-bound y)) 0)
+      (mul-interval x
+                    (make-interval (/ 1.0 (upper-bound y))
+                                   (/ 1.0 (lower-bound y))))
+                    (error "deviding by 0")))
+
+(define (add-interval x y)
+  (make-interval
+    (+ (lower-bound x) (lower-bound y))
+    (+ (upper-bound x) (upper-bound y))))
+
+(define (par1 r1 r2)
+  (div-interval (mul-interval r1 r2)
+                (add-interval r1 r2)))
+
+(define (par2 r1 r2)
+  (let ((one (make-interval 1 1)))
+    (div-interval one
+                  (add-interval (div-interval one r1)
+                                (div-interval one r2)))))
+
+(define (make-center-width c w)
+  (make-interval (- c w) (+ c w)))
+
+(define (center i)
+  (/ (+ (lower-bound i) (upper-bound i)) 2))
+
+(define (width i)
+  (/ (- (upper-bound i) (lower-bound i)) 2))
+
+
+(define (make-center-percent c p)
+  (make-center-width c (/ (* c p) 100)))
+
+(define (percent i) 
+  (/ (* 100 (width i)) (center i)))
+
+(define c1 (make-center-percent 10 0))
+
+(define c2 (make-center-percent 10 1))
