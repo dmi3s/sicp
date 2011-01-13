b13bfe2c1e9067731e9d180c66444254243a90de
dmi3s <dmi3evsv@gmail.com>
2011-01-13 12:12:53 +0300

    2.29 c

--- 2/2.29.scm
+++ 2/2.29.scm
@@ -21,27 +21,42 @@
 (define (branch-structure branch)
   (cadr branch))
 
+(define (branch-weight branch)
+  (let ((structure (branch-structure branch)))
+    (if (mobile? structure)
+        (total-weight structure)
+        structure)))
+
 (define (total-weight mobile)
-  (define (branch-weight branch)
-    (let ((structure (branch-structure branch)))
-      (if (mobile? structure)
-          (total-weight structure)
-          structure)))
   (+ (branch-weight (left-branch mobile))
      (branch-weight (right-branch mobile))))
-  
+
+(define (branch-moment branch)
+  (* (branch-length branch) (branch-weight branch)))
+
+(define (mobile-balanced? mobile)
+  (define left (left-branch mobile))
+  (define right (right-branch mobile))
+  (define (branch-balanced? branch)
+    (if (mobile? (branch-structure branch))
+        (mobile-balanced? (branch-structure branch))
+        't))
+  (and (branch-balanced? left)
+       (branch-balanced? right)
+       (= (branch-moment left) (branch-moment right))))
+
 (define test
   (make-mobile
-   (make-branch 1 2)
-   (make-branch 3 4)))
+   (make-branch 10 20)
+   (make-branch 20 10)))
 
 (define test-mob
   (make-mobile
-   (make-branch 10
+   (make-branch 100
                 (make-mobile
-                 (make-branch 12 5)
-                 (make-branch 33 2)))
-   (make-branch 18
+                 (make-branch 1 2)
+                 (make-branch 2 1)))
+   (make-branch 1
                 (make-mobile
-                 (make-branch 7 3)
-                 (make-branch 8 9)))))
+                 (make-branch 100 200)
+                 (make-branch 200 100)))))
