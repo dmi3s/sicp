8fde3b6806e863271b437b44abcaf43ccf636f37
dmi3s <dmi3evsv@gmail.com>
2011-01-13 16:26:10 +0300

    2.36

---
+++ 2/2.36.scm
@@ -0,0 +1,18 @@
+#lang planet neil/sicp
+
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
+
+(define (accumulate-n op init seqs)
+  (if (null? (car seqs))
+      nil
+      (cons (accumulate op init 
+                        (map car seqs))
+            (accumulate-n op init
+                          (map cdr seqs)))))
+
+(define test
+  '((1 2 3) (4 5 6) (7 8 9) (10 11 12)))
