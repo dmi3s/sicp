0c4ad8246d2f1cc610cf614e8371d8ee4e1dd92b
dmi3s <dmi3evsv@gmail.com>
2011-01-13 15:59:42 +0300

    2.34

---
+++ 2/2.34.scm
@@ -0,0 +1,16 @@
+#lang planet neil/sicp
+
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
+
+(define (horner-eval x coefficient-sequence)
+  (accumulate (lambda (this-coeff higher-terms)
+                (+ (* x higher-terms) this-coeff))
+              0
+              coefficient-sequence))
+
+; x^2 + 2x + 3
+(define coeffs '(3 2 1))
