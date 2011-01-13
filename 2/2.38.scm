96232f6b62a08faead6086f07d0477f475de4829
dmi3s <dmi3evsv@gmail.com>
2011-01-13 17:25:20 +0300

    2.38

---
+++ 2/2.38.scm
@@ -0,0 +1,15 @@
+#lang planet neil/sicp
+
+(define (fold-right op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (fold-right op initial (cdr sequence)))))
+
+(define (fold-left op initial sequence)
+  (define (iter result rest)
+    (if (null? rest)
+        result
+        (iter (op result (car rest))
+              (cdr rest))))
+  (iter initial sequence))
