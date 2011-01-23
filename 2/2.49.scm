fd0b7f1e4c1a59218604721bf71c866053c3337f
dmi3s <dmi3evsv@gmail.com>
2011-01-23 22:22:48 +0300

    2.49 c

--- 2/2.49.scm
+++ 2/2.49.scm
@@ -1,18 +1,17 @@
 #lang scheme
 (require (planet "sicp.ss" ("soegaard" "sicp.plt" 2 1)))
 
-(define (draw-contour fr)
-  (define lb (frame-origin fr))
-  (define lt (vector-add lb (frame-edge2 fr)))
-  (define rt (vector-add lt (frame-edge1 fr)))
-  (define rb (vector-add lb (frame-edge1 fr)))
-  (define segs (list 
-                (make-segment lb lt)
-                (make-segment lt rt)
-                (make-segment rt rb)
-                (make-segment rb lb)
-                ))
-  (segments->painter segs))
+(define ms make-segment)
+(define mv make-vect)
+
+(define draw-contour
+  (let ((segs (list 
+               (ms (mv 0 0) (mv 0 1))
+               (ms (mv 0 1) (mv 1 1))
+               (ms (mv 1 1) (mv 1 0))
+               (ms (mv 1 0) (mv 0 0))
+               )))
+    (segments->painter segs)))
 
 
 (define eps 0.1)
@@ -20,7 +19,24 @@
 (define fr0
   (make-frame (make-vect 0 0) (make-vect (- 1 eps) 0) (make-vect 0 (- 1 eps))))
 
-(define test-a (paint (draw-contour fr0)))
+(define test-a (paint draw-contour))
 
+(define draw-x
+  (let ((segs (list
+               (ms (mv 0 1) (mv 1 0))
+               (ms (mv 0 0) (mv 1 1))
+               )))
+  (segments->painter segs)))
 
-  
+(define test-b (paint draw-x))
+
+(define draw-diamond
+  (let ((segs (list
+               (ms (mv 0 0.5) (mv 0.5 1))
+               (ms (mv 0.5 1) (mv 1 0.5))
+               (ms (mv 1 0.5) (mv 0.5 0))
+               (ms (mv 0.5 0) (mv 0 0.5)))))
+  (segments->painter segs)))
+
+(define test-c (paint draw-diamond))
+
