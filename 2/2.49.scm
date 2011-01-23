ed4b14ec6cec903035d83af35117ba2db1f66c95
dmi3s <dmi3evsv@gmail.com>
2011-01-23 23:18:00 +0300

    2.49

--- 2/2.49.scm
+++ 2/2.49.scm
@@ -1,6 +1,5 @@
 #lang scheme
 (require (planet "sicp.ss" ("soegaard" "sicp.plt" 2 1)))
-(require "mods/accumulate.scm")
 
 (define ms make-segment)
 (define mv make-vect)
@@ -27,7 +26,7 @@
                (ms (mv 0 1) (mv 1 0))
                (ms (mv 0 0) (mv 1 1))
                )))
-  (segments->painter segs)))
+    (segments->painter segs)))
 
 (define test-b (paint draw-x))
 
@@ -37,25 +36,52 @@
                (ms (mv 0.5 1) (mv 1 0.5))
                (ms (mv 1 0.5) (mv 0.5 0))
                (ms (mv 0.5 0) (mv 0 0.5)))))
-  (segments->painter segs)))
+    (segments->painter segs)))
 
 (define test-c (paint draw-diamond))
 
 (define (polyline vertices)
-  (define (iter cur rest result)
-    (if (empty? rest)
-        result
-        (let ((next (car rest)))
-          (iter next (cdr rest) (cons (ms cur next) result)))))
-  (if (empty? vertices)
+  (if (empty? (cdr vertices))
       '()
-      (iter (car vertices) (cdr vertices) '())))
+      (cons (ms (car vertices) (cadr vertices)) (polyline (cdr vertices)))))
 
 (define (contour vertices)
-  (let ((line-segments (polyline vertices)))
-    (if (empty? line-segments)
-        '()
-        (cons (ms (cdar line-segments) (car vertices)) line-segments))))
+  (polyline (append vertices (list (car vertices)))))
 
 (define (test-contour)
   (paint (segments->painter (contour (list (mv 0 0) (mv 0 1) (mv 1 0))))))
+
+(define wave
+  (let ((segs (append
+               (polyline (list
+                          (make-vect 0 0.15)
+                          (make-vect 0.15 0.4)
+                          (make-vect 0.3 0.35)
+                          (make-vect 0.4 0.35)
+                          (make-vect 0.35 0.15)
+                          (make-vect 0.4 0)))
+               (polyline (list
+                          (make-vect 0.6 0)
+                          (make-vect 0.65 0.15)
+                          (make-vect 0.6 0.35)
+                          (make-vect 0.75 0.35)
+                          (make-vect 1 0.65)))
+               (polyline (list
+                          (make-vect 1 0.85)
+                          (make-vect 0.6 0.55)
+                          (make-vect 0.75 1)))
+               (polyline (list
+                          (make-vect 0.6 1)
+                          (make-vect 0.5 0.7)
+                          (make-vect 0.4 1)))
+               (polyline (list 
+                          (make-vect 0.25 1)
+                          (make-vect 0.35 0.5)
+                          (make-vect 0.3 0.4)
+                          (make-vect 0.15 0.6)
+                          (make-vect 0 0.35))))))
+    (segments->painter segs)))
+
+
+(define (test-d)
+  (paint-hires (flip-vert wave)))
