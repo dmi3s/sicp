1ae2bf33219bd547ed38a4f1584fb414580efb48
dmi3s <dmi3evsv@gmail.com>
2011-01-23 22:59:35 +0300

    2.49 polyline v0.1

--- 2/2.49.scm
+++ 2/2.49.scm
@@ -1,11 +1,12 @@
 #lang scheme
 (require (planet "sicp.ss" ("soegaard" "sicp.plt" 2 1)))
+(require "mods/accumulate.scm")
 
 (define ms make-segment)
 (define mv make-vect)
 
 (define draw-contour
-  (let ((segs (list 
+  (let ((segs (list
                (ms (mv 0 0) (mv 0 1))
                (ms (mv 0 1) (mv 1 1))
                (ms (mv 1 1) (mv 1 0))
@@ -40,3 +41,21 @@
 
 (define test-c (paint draw-diamond))
 
+(define (polyline vertices)
+  (define (iter cur rest result)
+    (if (empty? rest)
+        result
+        (let ((next (car rest)))
+          (iter next (cdr rest) (cons (ms cur next) result)))))
+  (if (empty? vertices)
+      '()
+      (iter (car vertices) (cdr vertices) '())))
+
+(define (contour vertices)
+  (let ((line-segments (polyline vertices)))
+    (if (empty? line-segments)
+        '()
+        (cons (ms (cdar line-segments) (car vertices)) line-segments))))
+
+(define (test-contour)
+  (paint (segments->painter (contour (list (mv 0 0) (mv 0 1) (mv 1 0))))))
