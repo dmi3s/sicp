14039034560060749c73f69408099264d688d335
dmi3s <dmi3evsv@gmail.com>
2011-01-24 13:27:47 +0300

    2.52 b

--- 2/2.52.scm
+++ 2/2.52.scm
@@ -24,6 +24,29 @@
                )))
     (segments->painter segs)))
 
+(define (test-a) (paint-hires wave))
 
-(define (test-a)
-  (paint-hires wave))
+(define (up-split painter n)
+  (if (= n 0)
+      painter
+      (let ((smaller (up-split painter (- n 1))))
+        (below painter (beside smaller smaller)))))
+
+(define (right-split painter n)
+  (if (= n 0)
+      painter
+      (let ((smaller (right-split painter (- n 1))))
+        (beside painter (below smaller smaller)))))
+
+(define (corner-split-mod painter n)
+  (if (= n 0)
+      painter
+      (let ((up (up-split painter (- n 1)))
+            (right (right-split painter (- n 1))))
+        (let ((top-left up)
+              (bottom-right right)
+              (corner (corner-split-mod painter (- n 1))))
+          (beside (below painter top-left)
+                  (below bottom-right corner))))))
+
+(define (test-b) (paint-hires (corner-split-mod wave 2)))
