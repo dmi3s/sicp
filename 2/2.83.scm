1c7a44574b811695445aed56b56723e038e29512
Sergey Dmitriev (dmi3s) <dmi3evsv@gmail.com>
2011-05-08 01:53:10 +0400

    Rackunit usage tested

--- 2/2.83.scm
+++ 2/2.83.scm
@@ -1,12 +1,18 @@
 #lang scheme
 (require "mods/math.scm"
          "mods/math-install.scm"
+         rackunit
          )
 
 (install-math)
 
-(define r (make-rational 1 2))
+(test-case "Raise of rational"
+           (define r (make-rational 1 7))
+           (check-equal? (raise r) (make-scheme-number (/ 1 7))))
 
-(define (test)
-  (raise (raise r)))
+(test-case "Double raise of rational"
+           (define r (make-rational 1 3))
+           (let ((double-raised-value (raise (raise r))))
+             (check-equal? (real-part double-raised-value) (/ 1 3))
+             (check-equal? (imag-part double-raised-value) 0)))
 
